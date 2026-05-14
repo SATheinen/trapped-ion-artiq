46952d075a99f9af627521e04da0132430c81ad1
Silas Theinen <silas.theinen@rwth-aachen.de.com>
2026-05-14 13:44:27 +0900

    Simulation device fixes

--- qubitcore_control/sim/devices.py
+++ qubitcore_control/sim/devices.py
@@ -4,7 +4,8 @@ from artiq.language.types import TFloat, TInt32
 
 class SimPMT:
 
-    def __init__(self):
+    def __init__(self, device_mgr):
+        self.device_mgr = device_mgr # Ignore
         self.ion = ion
 
     def count(self, duration: TFloat) -> TInt32:
