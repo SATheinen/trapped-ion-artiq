4641344baea7aff66ceac82764921cfad2afe9f9
Silas Theinen <silas.theinen@rwth-aachen.de.com>
2026-05-14 14:01:38 +0900

    Simulation device fixes

--- qubitcore_control/system/modules/detection.py
+++ qubitcore_control/system/modules/detection.py
@@ -4,6 +4,7 @@ from artiq.language.types import TFloat, TInt32
 class DetectionModule():
 
     def build(self, experiment):
+        self.core = experiment.core
         self._pmt = experiment.get_device("ttl_pmt")
 
     @kernel
