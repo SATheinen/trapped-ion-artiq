d2ae7c411a4dbf3abb5ea8010ca6a813946a9942
Silas Theinen <silas.theinen@rwth-aachen.de.com>
2026-05-14 13:39:01 +0900

    Simulation device fixes

--- qubitcore_control/sim/core.py
+++ qubitcore_control/sim/core.py
@@ -1,3 +1,9 @@
+# Dummy function we need in simulations only
 class Core:
+
+    def __init__(self, device_mgr):
+        self.device_mgr = device_mgr # Ignore
+
+    # Dummy function, do nothing
     def reset():
         return None
