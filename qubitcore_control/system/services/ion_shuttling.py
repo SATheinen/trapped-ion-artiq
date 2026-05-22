61dee1800d0faa12ec6741035910d6f5a483ace6
Silas Theinen <silas.theinen@rwth-aachen.de.com>
2026-05-22 19:02:55 +0200

    Add ion shuttling devices and tests

--- qubitcore_control/system/services/ion_shuttling.py
+++ qubitcore_control/system/services/ion_shuttling.py
@@ -1,29 +1,20 @@
 from artiq.experiment import kernel, delay, ms, us
 from artiq.language.types import TInt32, TFloat, TNone
-from system.modules.laser_397 import Laser397CoolModule, Laser397PumpModule
-from system.modules.laser_729 import Laser729Module
-from system.services.cooling import CoolingService
 import numpy as np
 
 class ShuttlingService:
 
-    def build(self, trap_dc):
-        self._trap_dc = trap_dc
-        self.laser_729 = Laser729Module()
-        self.laser_729.build(self)
-        self.laser_397_cool = Laser397CoolModule()
-        self.laser_397_cool.build(self)
-        self.laser_397_pump = Laser397PumpModule()
-        self.laser_397_pump.build(self)
-        self.core = trap_dc.core
+      def build(self, trap_dc, cooling):
+          self._trap_dc = trap_dc
+          self._cooling = cooling
+          self.core = trap_dc.core
 
-        self.cooling = CoolingService()
-        self.cooling.build(self.laser_729, self.laser_397_cool, self.laser_397_pump)
+      @kernel
+      def shuttle(self, ion_index: TInt32, to_z: TInt32) -> TNone:
+          self._trap_dc.shuttle(ion_index, to_z)
 
-    def shuttle(self, ion_index, to_z):
-        self._trap_dc.shuttle(ion_index, to_z)
-    
-    def shuttle_and_recool(self, ion_index, to_z):
-        self._trap_dc.shuttle(ion_index, to_z)
-        self.cooling.doppler_cool()
-        self.cooling.full_automatic_sideband_cool()
+      @kernel
+      def shuttle_and_recool(self, ion_index: TInt32, to_z: TInt32) -> TNone:
+          self._trap_dc.shuttle(ion_index, to_z)
+          self._cooling.doppler_cool()
+          self._cooling.full_automatic_sideband_cool()
