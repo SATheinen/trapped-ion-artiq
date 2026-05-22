33703f326e9a81e04690549a4d728bee93a8bf75
Silas Theinen <silas.theinen@rwth-aachen.de.com>
2026-05-22 18:34:48 +0200

    system providing convenient ion moevemnt combined with cooling

---
+++ qubitcore_control/system/services/ion_shuttling.py
@@ -0,0 +1,28 @@
+from artiq.experiment import kernel, delay, ms, us
+from artiq.language.types import TInt32, TFloat, TNone
+from system.modules.laser_397 import Laser397CoolModule, Laser397PumpModule
+from system.modules.laser_729 import Laser729Module
+from system.services.cooling import CoolingService
+import numpy as np
+
+class ShuttlingService:
+
+    def build(self, trap_dc):
+        self._trap_dc = trap_dc
+        self.laser_729 = Laser729Module()
+        self.laser_729.build(self)
+        self.laser_397_cool = Laser397CoolModule()
+        self.laser_397_cool.build(self)
+        self.laser_397_pump = Laser397PumpModule()
+        self.laser_397_pump.build(self)
+
+        self.cooling = CoolingService()
+        self.cooling.build(self.laser_729, self.laser_397_cool, self.laser_397_pump)
+
+    def shuttle(self, ion_index, to_z):
+        self._trap_dc.shuttle(ion_index, to_z)
+    
+    def shuttle_and_recool(self, ion_index, to_z):
+        self._trap_dc.shuttle(ion_index, to_z)
+        self.cooling.doppler_cool()
+        self.cooling.full_automatic_sideband_cool()
