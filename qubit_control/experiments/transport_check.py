c8be0401bd7097360294e437dba96bdf31d05b92
Silas Theinen <silas.theinen@rwth-aachen.de.com>
2026-06-18 23:58:21 +0200

    add verifier experiment to test, if ions block each other transport_check.py

--- qubit_control/experiments/transport_check.py
+++ qubit_control/experiments/transport_check.py
@@ -1,4 +1,4 @@
-from artiq.experiment import kernel, delay, ms, us
+from artiq.experiment import EnvExperiment, kernel, delay, NumberValue, us
 from artiq.language.types import TInt32, TFloat, TNone
 from system.modules.laser_729 import Laser729Module
 from system.modules.laser_397 import Laser397CoolModule, Laser397PumpModule
